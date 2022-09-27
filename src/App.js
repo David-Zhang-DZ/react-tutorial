5dd0c6be1fe39a9126b9aee7cf375178d7a05545
David Zhang <davidzhang2023@u.northwestern.edu>
2022-09-27 13:45:40 -0500

    fetch data

--- src/App.js
+++ src/App.js
@@ -3,42 +3,28 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Banner from './components/Banner';
 import CourseList from './components/CourseList';
+import { useJsonQuery } from './utilities/fetch';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
+const queryClient = new QueryClient();
 
 const App = () => (
-  <>
+  <QueryClientProvider client={queryClient}>
+    <AppMain/>
+  </QueryClientProvider>
+)
+
+const AppMain = () => {
+  const [schedule, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+
+  if(isLoading) return <h1>Loading Data</h1>;
+  if(error) return <h1>Error Loading Data</h1>;
+
+  return <> 
     <Banner title={schedule.title}></Banner>
     <CourseList courses={schedule.courses}></CourseList>
-  </> 
-);
+    </>;
+}
+  
 
 export default App;
--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -2,7 +2,7 @@ import Course from "./Course";
 
 const CourseList = ({courses}) => (
     <div className="course-list">
-        {Object.entries(courses).map(([id, course]) => <Course key={course.number} course={course}></Course>)}
+        {Object.entries(courses).map(([id, course]) => <Course key={id} course={course}></Course>)}
     </div>
 );
 
