ef6c1320fb2840d319a4ff7b75d765cb9d5dcb29
David Zhang <davidzhang2023@u.northwestern.edu>
2022-11-08 23:18:12 -0600

    added content test

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,5 +1,12 @@
+/* globals cy */
+
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('/')
-  })
+  });
+
+  it ('opens with Fall CS courses', () => {
+    cy.visit ('/');
+    cy.get('[data-cy=course]').should('contain', 'Fall CS');
+  });
 })
--- src/components/Course.js
+++ src/components/Course.js
@@ -5,7 +5,9 @@ import "../App.css";
 const Course = ({id, course, selected, toggleSelected, profile}) => {
   console.log(profile);
 
-  return <div className="course card m-1 p-2" onClick={() => toggleSelected(id)}>
+  return <div className="course card m-1 p-2" 
+              data-cy="course"
+              onClick={() => toggleSelected(id)}>
     <div className={`card-body ${selected.includes(id) ? 'selected' : ''}`}>
       <h5 className="card-title">{course.term} CS {course.number}</h5>
       <p className="card-text">{course.title}</p>
